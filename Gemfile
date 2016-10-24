07e850ce1b3db2c91540be0e77a9d941f8801c40
Takafumi ONAKA <takafumi.onaka@gmail.com>
2016-10-24 16:46:05 +0900

    it works

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+gem "dalli", require: "rack/session/dalli"
+gem "reverse_markdown"
+gem "sinatra", require: "sinatra/base"
+gem "sinatra-contrib"
+gem "slim"
+
+group :development do
+  gem "pry"
+  gem "pry-doc"
+end
---
+++ app.rb
@@ -0,0 +1,32 @@
+class App < Sinatra::Base
+  register Sinatra::MultiRoute
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  configure do
+    enable :logging
+    file = File.new("#{settings.root}/log/#{settings.environment}.log", "a+")
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
+  use Rack::Session::Dalli, {
+    namespace: "html2slim.session",
+    key: "_html2slim_session",
+    expire_after: 2_592_000, # 30 days
+  }
+
+  route :get, :post, "/" do
+    if params[:source]
+      session[:source] = params[:source]
+      session[:target] = ReverseMarkdown.convert(session[:source], github_flavored: true)
+      redirect to("/")
+    end
+
+    @source = session[:source]
+    @target = session[:target]
+    session.clear
+    slim :index
+  end
+end
